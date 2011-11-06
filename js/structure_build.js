8792d4e15f16548801947fe7bdcac23320e374c7
Calvin Tennant <calvintennant@gmail.com>
2011-11-06 16:58:24 -0500

    process of seperating 2d from 3d so that non webgl browsers can still manipulate structures

--- js/structure_build.js
+++ js/structure_build.js
@@ -1,293 +1,232 @@
 (function ($) {
-    $(document).ready(function(){
-
-initiate3d();
-animate();
-line = drawGrid(scene, 0);
-
-function addBlock(blocks,block) {
-  for(var i=0; blocks[i]; i++) {
-    if (block[0] == blocks[i][0]
-        && block[1] == blocks[i][1]
-        && block[2] == blocks[i][2]
-        && block[3] == blocks[i][3]) {
-        console.log('nothing to add');
-        return blocks;
-    }
-    else if (block[0] == blocks[i][0]
+  $(document).ready(function(){
+    
+   /* Add array block to array blocks
+    * 
+    * @param blocks as array
+    * @param block as array
+    * @return blocks as array
+    */
+    function addBlock(blocks,block) {
+      
+      for(var i=0; blocks[i]; i++) {
+       /* 
+        * First run through all items in blocks array and 
+        * make sure that block is unique
+        */
+        if (block[0] == blocks[i][0]
+          && block[1] == blocks[i][1]
+          && block[2] == blocks[i][2]
+          && block[3] == blocks[i][3]) {
+          console.log('nothing to add');
+          return blocks;
+        }
+       /* 
+        * if block is unique make sure that there aren't any other blocks
+        * with the same coordinates, if there are remove them
+        */
+        else if (block[0] == blocks[i][0]
             && block[1] == blocks[i][1]
             && block[2] == blocks[i][2]) {
-        console.log('killing block..');
-        scene.remove(blocks[i][4]);
-        blocks.splice(i,1);
-        break;
-    }
-  }
-  if (block[3] !=  0) {
-    size = 16;
-    
-    console.log('drawing block..');
-    var CubeMaterial = blockTexture(block[3]);
-    
-    //create cube
-    var Cube = new THREE.Mesh(
-    new THREE.CubeGeometry(size,size,size,1,1,1,CubeMaterial), new THREE.MeshLambertMaterial());  
-    //position
-    Cube.position['y'] = (block[0]*size);
-    Cube.position['x'] = (block[1]-8)*size+8;
-    Cube.position['z'] = (block[2]-8)*-size-8;
-
-    //draw
-    scene.add(Cube);
-    
-    block.push(Cube);
-
-    console.log('adding block..');
-    blocks.push(block);
-
-  }
-    
-  return blocks;
-}
-
-function drawControls(Z, X, Y, blocks) {
-    if ($('#structure-node-form .xy-grid .mc-block').length == 0) {
-    /*
-        * Create 256 divs for each clickable block position
-        */
-    $('#structure-node-form .xy-grid').empty();
-    zcount = Z;
-    xcount = X;
-    ycount = Y;
-    for (var i=0;i<=255;i++) {
-        $('#structure-node-form .xy-grid').append('\n\
-        <div class="mc-block air" id="Z'+ zcount + '_X'+ xcount + '_Y' + ycount + '">\n\
-        ');
-
-        if (xcount < X+15) {
-        xcount++;
+          console.log('killing block..');
+          blocks.splice(i,1);
+          break;
         }
-        else if (ycount < Y+15) {
-        xcount = X;
-        ycount ++;
-        }
-        else {
-        xcount = X;
-        ycount = Y;
-        zcount --;
-        }
-    }
+      }
+      
+     /*
+      * ensure that the block we're about to add isn't a 0 block (air)
+      */
+      if (block[3] !=  0) {
+        console.log('adding block..');
+        blocks.push(block);
+      }
+      return blocks;
     }
     
-    zcount = Z;
-    xcount = X;
-    ycount = Y;
-    $('#structure-node-form .xy-grid .mc-block').each(function() {
-        block = identifyBlock(this);
+   /* Draw user interface controls on the screen
+    * 
+    * @param current Z position as number
+    * @param current X position as number
+    * @param current Y position as number
+    * @param blocks as array
+    */
+    function drawControls(Z, X, Y, blocks) {
+      console.log('drawing controls');
+     /* If there aren't any .xy-grid .mc-block elements then it's our first
+      * time through, create them
+      */
+      if ($('#structure-node-form .xy-grid .mc-block').length == 0) {
+        zcount = Z;xcount = X;ycount = Y;
+        for (var i=0;i<=255;i++) {
+          $('#structure-node-form .xy-grid').append('<div class="mc-block air" id="Z'+ zcount + '_X'+ xcount + '_Y' + ycount + '">');
+          if (xcount < X+15) { xcount++; }
+          else if (ycount < Y+15) { xcount = X;ycount ++; }
+          else { xcount = X;ycount = Y;zcount --; }
+        }
+      }
+      
+     // Identify each .xy-grid .mc-block element and remove style attribute
+      zcount = Z;xcount = X;ycount = Y;
+      $('#structure-node-form .xy-grid .mc-block').each(function() {
         $(this).attr('id', 'Z'+ zcount + '_X'+ xcount + '_Y' + ycount);
-        
-        if (xcount < X+15) {
-        xcount++;
-        }
-        else if (ycount < Y+15) {
-        xcount = X;
-        ycount ++;
-        }
-        else {
-        xcount = X;
-        ycount = Y;
-        zcount --;
-        }
-        
+
+        if (xcount < X+15) { xcount++; }
+        else if (ycount < Y+15) { xcount = X; ycount ++; }
+        else { xcount = X; ycount = Y; zcount --; }
+
         $(this).removeAttr('style');
-    });
-    /*
-        * apply approproate styling to corrosponding .mc-block
-        */
-    for (var i = 0; blocks[i]; i++) {
-        if($('#Z'+ blocks[i][0] + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).length == 1){
+      });
+      
+     // loop through blocks and style .xy-grid .mc-blocks
+      for (var i = 0; blocks[i]; i++) {
+         // If there is a matching .xy-grid .mc-blocks element style it
+          if($('#Z'+ blocks[i][0] + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).length == 1){
             $('#Z'+ blocks[i][0] + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).attr('style', '\n\
             background-image: url(\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + blocks[i][3] + '.png\');\n\
-            opacity: 1;\n\
-        ');
-        }
-        else {
-        for(var j = 0; j<4; j++) {
-            if($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).length == 1 &&
-            (!($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).attr('style')) ||
-            ($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).css('opacity')<(.5 - (j/10))))) {
-                $('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).attr('style', '\n\
+            opacity: 1;');
+          }
+         /* if there is not a matching .xy-grid .mc-blocks element, see if
+          * there is a matching element on a lower Z-level and draw it in 
+          * with a slightly lesser opacity
+          */
+          else {
+          for(var j = 0; j<4; j++) {
+              if  ($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).length == 1 &&
+                (!($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).attr('style')) ||
+                  ($('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).css('opacity')<(.5 - (j/10))))) {
+                  $('#Z'+ (Number(blocks[i][0])+j) + '_X'+ blocks[i][1] + '_Y' + blocks[i][2]).attr('style', '\n\
                 background-image: url(\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + blocks[i][3] + '.png\');\n\
-                opacity: ' + (.5 - (j/10)) + ' ;\n\
-            ');
-            break;
+                opacity: ' + (.5 - (j/10)) + ' ;');
+              break;
+            }
           }
         }
       }
     }
-}
-
-function identifyBlock(thisblock) {
-    //isolate the coordinates
-    id = $(thisblock).attr("id");
-    coords = id.replace(/[^0-9-_.]/g, "");
-    coords = coords.split("_");
-    //console.log('Z:' + coords[0],' X:' + coords[1],' Y:' + coords[2]);
-    return new Array (coords[0],coords[1],coords[2],$('input:radio[name=block_type]:checked').val());
-}
-
-function loadblocks(blockstr) {
-    var blocks = new Array();
-
-    //split up and loop through each block
-    for (var i = 0, block; block = blockstr.split(",")[i]; i++) {
-
-      //isolate the coordinates
-      coords = block.split("][");
-      coords = block.replace(/[^0-9.]/g, "");
-
-      //isolate the block type
-      blocktype = block.replace(/[^a-z.]/g, "");
     
-      block = block.replace(/[[]/g, '');
-      block = block.split(']');
-      blocks[i] = block;
-    }
-    return blocks;
-}
-
-var blocks = new Array;
-var blockstr = $('#edit-field-structurearray-und-0-value').val();
-$('#edit-field-structurearray-und-0-value').val('');
-if (blockstr) {
-    blocks = loadblocks(blockstr);
-}
-
-var currentZ = new Number;
-var currentX = new Number;
-var currentY = new Number;
-var currentZoom = new Number;
-currentZ = 0;
-currentX = 0;
-currentY = 0;
-currentZoom = 0;
-drawControls(currentZ, currentX, currentY, blocks);  
-gridControl();
-
-$('.up-z').click(function() {
-    console.log('up');
-    currentZ++;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('.down-z').click(function() {
-    console.log('down');
-    currentZ--;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('.north-y').click(function() {
-    console.log('north');
-    currentY--;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('.south-y').click(function() {
-    console.log('south');
-    currentY++;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('.east-x').click(function() {
-    console.log('east');
-    currentX++;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('.west-x').click(function() {
-    console.log('west');
-    currentX--;
-    drawControls(currentZ, currentX, currentY, blocks);  
-    gridControl();
-    return false;
-});
-
-$('#edit-submit').click(function() {
-  var blockstr = new String;
-  $('#edit-field-structurearray-und-0-value').val('');
-  for (var i = 0, block; block = blocks[i]; i++) {
-    for (var j = 0; block[j]; j++) {
-      block[j] = '[' + block[j] + ']';
-    }
-  blockstr += block.slice(0,4).toString().replace(/,/g, '') + ',';
-  }
-  $('#edit-field-structurearray-und-0-value').val(blockstr);
-});
-
-$('#mc-3d .zoom-in').click(function() {
-    console.log('zooming in' + currentZoom);
-    currentZoom -= 50;
-    camera.position.set( 0, currentZoom + 200, 200);
-});
-
-$('#mc-3d .zoom-out').click(function() {
-    console.log('zooming out ' + currentZoom);
-    currentZoom += 50;
-    camera.position.set( 0, currentZoom + 200, 200);
-});
-
-$('#structure-node-form .xy-grid').selectable({
-    /*
-    selecting: function(event, ui){ 
-    $(".ui-selecting", this).each(function(){
-        $(this).css('background-image', 'url(' + '\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + $('input:radio[name=block_type]:checked').val() + '.png\')');
-        $(this).css('opacity', '1');
-    });
-    },
+   /* Provide .xy-grid .mc-block element and return it as block array
+    * 
+    * @param thisblock as HTML element
+    * @return new Array as structured block (X,Y,Z,TYPE)
     */
-    selected: function(event, ui){ 
-    $(".ui-selected", this).each(function(){
-        $(this).css('background-image', 'url(' + '\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + $('input:radio[name=block_type]:checked').val() + '.png\')');
-        $(this).css('opacity', '1');
-        type = $('input:radio[name=block_type]:checked').val();
-        $(this).attr('class', 'mc-block '+ type);
-        var block = identifyBlock(this);
-        blocks = addBlock(blocks,block);
-        return blocks;
-    });
+    function identifyBlock(thisblock) {
+      //isolate the coordinates
+      id = $(thisblock).attr("id");
+      coords = id.replace(/[^0-9-_.]/g, "");
+      coords = coords.split("_");
+      return new Array (coords[0],coords[1],coords[2],$('input:radio[name=block_type]:checked').val());
     }
-});
-function gridControl() {
-    /*
-    $('#structure-node-form .xy-grid .mc-block').click(function() {
-    type = $('input:radio[name=block_type]:checked').val();
-    $(this).attr('class', 'mc-block '+ type);
-    var block = identifyBlock(this);
-    blocks = addBlock(scene,blocks,block);
-    $(this).css('background-image', 'url(' + '\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + $('input:radio[name=block_type]:checked').val() + '.png\')');
-    $(this).css('opacity', '1');
-    })
-    $('#structure-node-form .xy-grid .mc-block').hover(function() {
-    if (mouseDown) {
-        type = $('input:radio[name=block_type]:checked').val();
-        $(this).attr('class', 'mc-block '+ type);
-        var block = identifyBlock(this);
-        blocks = addBlock(scene,blocks,block);
-        $(this).css('background-image', 'url(' + '\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + $('input:radio[name=block_type]:checked').val() + '.png\')');
-        $(this).css('opacity', '1');
-    }
-    });
+    
+   /* Provide blocks as array from document as string. This function will
+    * be used to deserialize blocks data from the database
+    * 
+    * @return blocks as array
     */
-}  
+    function loadBlocks() {
+      var blocks = new Array();
+      
+      // load blocks from page element
+      var blockstr = $('#edit-field-structurearray-und-0-value').val();
+      $('#edit-field-structurearray-und-0-value').val('');
+      
+      // if element provided data..
+      if (blockstr) {
+       /* Split up blockstr and loop through creating our
+        * multi-dimensional blocks array
+        */
+        for (var i = 0, block; block = blockstr.split(",")[i]; i++) {
+          block = block.replace(/[[]/g, '');
+          block = block.split(']');
+          blocks[i] = block;
+        }
+      }
+      return blocks;
+    }
+
+    var blocks = loadBlocks();
+    var currentZ = new Number;
+    var currentX = new Number;
+    var currentY = new Number;
+    drawControls(currentZ, currentX, currentY, blocks);  
+
+    $('.up-z').click(function() {
+      console.log('up');
+      currentZ++;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
     });
+
+    $('.down-z').click(function() {
+      console.log('down');
+      currentZ--;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
+    });
+
+    $('.north-y').click(function() {
+      console.log('north');
+      currentY--;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
+    });
+
+    $('.south-y').click(function() {
+      console.log('south');
+      currentY++;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
+    });
+
+    $('.east-x').click(function() {
+      console.log('east');
+      currentX++;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
+    });
+
+    $('.west-x').click(function() {
+      console.log('west');
+      currentX--;
+      drawControls(currentZ, currentX, currentY, blocks);  
+      return false;
+    });
+
+    $('#edit-submit').click(function() {
+      var blockstr = new String;
+      $('#edit-field-structurearray-und-0-value').val('');
+      for (var i = 0, block; block = blocks[i]; i++) {
+        for (var j = 0; block[j]; j++) {
+          block[j] = '[' + block[j] + ']';
+        }
+      blockstr += block.slice(0,4).toString().replace(/,/g, '') + ',';
+      }
+      $('#edit-field-structurearray-und-0-value').val(blockstr);
+    });
+
+    $('#structure-node-form .xy-grid').selectable({
+      /*
+      selecting: function(event, ui){ 
+      $(".ui-selecting", this).each(function(){
+          $(this).css('background-image', 'url(' + '\'/' + Drupal.settings.structurePath + '/sprites/mc-sprite_' + $('input:radio[name=block_type]:checked').val() + '.png\')');
+          $(this).css('opacity', '1');
+      });
+      },
+      */
+      selected: function(event, ui){ 
+        $(".ui-selected", this).each(function(){
+          block = identifyBlock(this);
+          type = $('input:radio[name=block_type]:checked').val();
+          $(this).attr('class', 'mc-block '+ type);
+          blocks = addBlock(blocks,block);
+          return blocks;
+        });
+      },
+      
+      stop: function(event, ui){
+        drawControls(currentZ, currentX, currentY, blocks);
+      }
+    });
+
+  });
 })(jQuery); 
